a3ad40560924d5b359fb9cc9803c591e8163397b
FireNH <FireNH@outlook.com>
2022-06-22 15:50:35 -0400

    removed withering from nether fortress chests

--- src/main/java/fireopal/enchantedexpanded/gameplay/AddToLootTable.java
+++ src/main/java/fireopal/enchantedexpanded/gameplay/AddToLootTable.java
@@ -1,29 +1,22 @@
 package fireopal.enchantedexpanded.gameplay;
 
-import fireopal.enchantedexpanded.enchantments.EEEnchantments;
-import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
-import net.minecraft.item.Items;
-import net.minecraft.loot.LootPool;
-import net.minecraft.loot.condition.RandomChanceLootCondition;
-import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.loot.function.EnchantRandomlyLootFunction;
 import net.minecraft.util.Identifier;
 
 public class AddToLootTable {
     public static final Identifier NETHER_FORTRESS_LOOT_TABLE_ID = new Identifier("chests/nether_bridge");
 
     public static void init() {
-        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
-            if (source.isBuiltin() && NETHER_FORTRESS_LOOT_TABLE_ID.equals(id)) {
-                LootPool.Builder poolBuilder = LootPool.builder()
-                    .with(
-                        ItemEntry.builder(Items.BOOK)
-                            .conditionally(RandomChanceLootCondition.builder(0.2f))
-                            .apply(new EnchantRandomlyLootFunction.Builder().add(EEEnchantments.WITHERING))
-                    );
+        // LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+        //     if (source.isBuiltin() && NETHER_FORTRESS_LOOT_TABLE_ID.equals(id)) {
+        //         LootPool.Builder poolBuilder = LootPool.builder()
+        //             .with(
+        //                 ItemEntry.builder(Items.BOOK)
+        //                     .conditionally(RandomChanceLootCondition.builder(0.2f))
+        //                     .apply(new EnchantRandomlyLootFunction.Builder().add(EEEnchantments.WITHERING))
+        //             );
         
-                tableBuilder.pool(poolBuilder);
-            }
-        });
+        //         tableBuilder.pool(poolBuilder);
+        //     }
+        // });
     }
 }
